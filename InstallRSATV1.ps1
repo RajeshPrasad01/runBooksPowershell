76bec8550f3e785d2dfe061f5b9334168a1671a6
RajeshPrasad01 <49905892+RajeshPrasad01@users.noreply.github.com>
2019-08-29 20:26:28 +0530

    Add files via upload
    V1.1

--- InstallRSATV1.ps1
+++ InstallRSATV1.ps1
@@ -1,19 +1,19 @@
-﻿$myAutomationConnection = Get-AutomationConnection -Name AzureRunAsConnection
+﻿
+$myAutomationConnection = Get-AutomationConnection -Name AzureRunAsConnection
 Connect-AzureRmAccount -CertificateThumbprint $myAutomationConnection.CertificateThumbprint -ApplicationId $myAutomationConnection.ApplicationId -ServicePrincipal -Tenant $myAutomationConnection.TenantId -Subscription $myAutomationConnection.SubscriptionId
+
 $myAuzureContext = Select-AzureRmSubscription -Subscription $myAutomationConnection.SubscriptionId
 
-### Install RSAT 
+$myLocalCred = Get-AutomationPSCredential -Name "myLocalCred"
 
-$myRSATState = Get-WindowsFeature -Name RSAT 
+$myRSATState = Get-WindowsFeature -Name RSAT -Credential $myLocalCred
 If ($myRSATState.InstallState -eq "Available") {
-Install-WindowsFeature -Name RSAT -IncludeAllSubFeature 
+Install-WindowsFeature -Name RSAT -IncludeAllSubFeature -Credential $myLocalCred
 } else {
 write-ouput "RSAT Feature not Available, Cant Install" 
-$myRSATState = Get-WindowsFeature -Name RSAT 
+$myRSATState = Get-WindowsFeature -Name RSAT -Credential $myLocalCred
 If ($myRSATState.Installed -eq $True) { 
 Write-output "Installed Successfully"
 } else {
 Write-output "Failed To Install" 
 }
-
-
