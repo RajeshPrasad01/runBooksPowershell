3696ca597e9a400b262d74923e6e240670b9898c
RajeshPrasad01 <49905892+RajeshPrasad01@users.noreply.github.com>
2019-08-29 19:24:50 +0530

    Add files via upload
    Start Stop VM

---
+++ VmStartStopV1.ps1
@@ -0,0 +1,23 @@
+﻿
+$myAutomationConnection = Get-AutomationConnection -Name AzureRunAsConnection
+Connect-AzureRmAccount -CertificateThumbprint $myAutomationConnection.CertificateThumbprint -ApplicationId $myAutomationConnection.ApplicationId -ServicePrincipal -Tenant $myAutomationConnection.TenantId -Subscription $myAutomationConnection.SubscriptionId
+
+$myAuzureContext = Select-AzureRmSubscription -Subscription $myAutomationConnection.SubscriptionId
+
+$myTestRG = New-AzureRmResourceGroup -Name TestRG -Location EastUs2
+$myVMs = Get-AzureRmVm -ResourceGroupName TestRG 
+Foreach ($myVm in $myVMs.Name) {
+
+Write-Output "Stopping Vm .." $myVm
+Stop-AzureRmVm -Name $myVM -ResourceGroupName TestRG -StayProvisioned -Force 
+$myVmStatus = Get-AzureRmVm -ResourceGroupName TestRG -Name $myVm -Status 
+
+If ($myVmStatus.Statuses[1].DisplayStatus -ne "VM running") { 
+Write-Output $myVm  $myVmStatus.Statuses[1].DisplayStatus
+} else {
+Write-output "Vm failed to stopped .." $myVm $myVmStatus.Statuses[1].DisplayStatus
+}
+}
+
+
+
